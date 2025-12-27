cc767889c724802c8162978a8f9b790aa861a068
oktavianuu <oktavianuss.misro@gmail.com>
2025-12-27 22:31:37 +0700

    cool

--- random-walk.c
+++ random-walk.c
@@ -1,9 +1,35 @@
 #include<SDL2/SDL.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 #define WIDTH 900
 #define HEIGHT 600
+
+#define SCALE 10;
+
+typedef struct {
+    int vx, vy;
+} Velocity;
+
+Velocity get_rand_v()
+{
+    int choice = rand() / (RAND_MAX/4);
+    printf("%d\n", choice);
+    switch(choice)
+    {
+        case 0: //up
+            return (Velocity) {0, -1}; 
+        case 1: //down
+            return (Velocity) {0, 1};
+        case 2: //left
+            return (Velocity) {-1, 0};
+        case 3: //right
+            return (Velocity) {1, 0};
+    }
+    fprintf(stderr, "Impossible random vale %d encountered\n", choice);
+    exit(-1);
+}
 
 int main(int argcount, const char *argval[])
 {
@@ -18,14 +44,16 @@ int main(int argcount, const char *argval[])
     }
     else
     {
-    printf("Usage: %s <num-of-agents>\n", argval[0]);
+        printf("Usage: %s <num-of-agents>\n", argval[0]);
+        return -1;
     }
+
+    srand(time(NULL));
 
     SDL_Window *pwindow = SDL_CreateWindow("Random Walk", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0); 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-    SDL_Rect rect = (SDL_Rect) {50, 50, 50, 50};
-    SDL_FillRect(psurface, &rect, 0xFFFFFF);
+    SDL_Rect rect = (SDL_Rect) {50, 50, 2, 2};
 
     int app_running = 1;
     while(app_running)
@@ -38,5 +66,12 @@ int main(int argcount, const char *argval[])
                 app_running = 0;
             }
         }
+
+        Velocity vel = get_rand_v();
+        rect.x += vel.vx * SCALE;
+        rect.y += vel.vy * SCALE;
+        SDL_FillRect(psurface, &rect, 0xFFFFFF);
+        SDL_UpdateWindowSurface(pwindow);
+        SDL_Delay(20);
     }
 }
